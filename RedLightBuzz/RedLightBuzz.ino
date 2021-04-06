75a404c2d7bbdd30f523ce502c1d6b05ab30f410
xDomanik <johnsonldomanik@gmail.com>
2021-04-05 21:57:08 -0400

    Added Redbuzz

--- RedLightBuzz/RedLightBuzz.ino
+++ RedLightBuzz/RedLightBuzz.ino
@@ -1,9 +1,40 @@
-void setup() {
-  // put your setup code here, to run once:
+int ledPin = 7;
+int buttonApin = 3;
+int buzzer = 8;
 
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonApin, INPUT_PULLUP);  
+  pinMode(buzzer, OUTPUT);
+  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  if (digitalRead(buttonApin) == HIGH)
+  {
+      Serial.print("HIGH");
 
-}
+      // Buzzer
+      int total = 10000;
+      int counter = 0;
+      
+      while (counter < total){
+        digitalWrite(buzzer, HIGH);
+        counter+=1;
+      }
+
+    digitalWrite(ledPin, HIGH);
+  }
+
+
+ if (digitalRead(buttonApin) == LOW)
+  {
+      Serial.print("LOW ");
+      digitalWrite(buzzer, LOW);
+
+    digitalWrite(ledPin, LOW);
+  }
+} 
